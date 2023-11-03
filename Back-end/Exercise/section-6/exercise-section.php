a4e0f102de51fbd6909387c637088e890bfba060
Hiệu Nguyễn <nguyenthehieua2001@gmail.com>
2023-11-03 15:21:25 +0700

    code 1

--- Back-end/Exercise/section-6/exercise-section.php
+++ Back-end/Exercise/section-6/exercise-section.php
@@ -40,10 +40,31 @@
 
 #Bài tập 3
 //T2 = 1/2 + 2/3 + .. + n/n+1 (n>=1)
-// Cách 1
-$t2 = 0;
-$n = 2;
-for($i = 1 ; $i <= $n; $i++ ){
-        $t2 +=($i/($i+1));
+//$t2 = 0;
+//$n = 2;
+//for($i = 1 ; $i <= $n; $i++ ){
+//        $t2 +=($i/($i+1));
+//}
+//echo "Tổng các số trong dẫy số là {$t2}";
+
+#Bài tập 3
+//Giải phương trình bặc 2 : ax^2 + bx + c = 0
+$a = 1;
+$b = 1;
+$c = -2;
+
+if ($a != 0) {
+    $delta = $b * $b - 4 * $a * $c;
+    if ($delta < 0) {
+        echo "Phương trình vô số nghiệm ";
+    } elseif ($delta == 0) {
+        $x = -$b / 2 * $a;
+        echo "Phương trình có nghiệm kép: x = {$x}";
+    } else {
+        $x1 = (-$b + sqrt($delta)) / 2 * $a;
+        $x2 = (-$b - sqrt($delta)) / 2 * $a;
+        echo"Phương trình có 2 nghiệm :<br> x1 : {$x1} <br> x2 : {$x2}";
+    }
+} else {
+    echo "Đây không phải phương trình bậc 2";
 }
-echo "Tổng các số trong dẫy số là {$t2}";
